b2f8a21edb725822bcf23a9446431734e97d304e
Tunde <tee_fats@yahoo.com>
2020-11-29 21:37:35 +0000

    Added login logic

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,9 +30,10 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'users.apps.UsersConfig',
+INSTALLED_APPS = [    
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL ='blog-home'
