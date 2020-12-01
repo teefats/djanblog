6a3e7c4ee39638f6663c0b0202179a98f0b7a096
Tunde <tee_fats@yahoo.com>
2020-12-01 19:40:15 +0000

    Updating user profile

--- users/apps.py
+++ users/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+# import users
 
 
 class UsersConfig(AppConfig):
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +8,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}\'s Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img =Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size =(300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
