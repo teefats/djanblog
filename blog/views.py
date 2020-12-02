5143de635889684d4cb79772dd35f34726f9c270
Tunde <tee_fats@yahoo.com>
2020-12-02 21:39:31 +0000

    added delete functionality

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 # posts = [{
@@ -39,6 +40,41 @@ class PostDetailView(DetailView):
     # #<app>/<model><viewtype>.html
     # context_object_name = 'posts'
     # ordering = ['-date_posted']
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class PostDeleteView(LoginRequiredMixin,
+                    UserPassesTestMixin,
+                    DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 
 def contact_us(request):
